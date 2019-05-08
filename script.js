a514f761fad317b1d96df099d0842e52701ea51e
Hjkun77 <38162086+Hjkun77@users.noreply.github.com>
2019-05-08 22:01:57 +0800

    Release 1.0.0

---
+++ script.js
@@ -0,0 +1,31 @@
+function toggleStickers() {
+  alert('Add Stickers to cart')
+}
+
+function toggleMug() {
+  alert('Add Mugs to cart')
+}
+
+function toggleLogoShirt() {
+  alert('Add Logo Shirt to cart')
+}
+
+function toggleHoodie() {
+  alert('Add Hoodie to cart')
+}
+
+function toggleBackpack() {
+  alert('Add Hoodie to cart')
+}
+
+function toggleMask() {
+  alert('Add Hoodie to cart')
+}
+
+function togglePhoneCase() {
+  alert('Add Hoodie to cart')
+}
+
+function toggleNotebook() {
+  alert('Add Hoodie to cart')
+}
